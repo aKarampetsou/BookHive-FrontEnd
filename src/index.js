3da9f7f7e2d5a492265b7c1205678b50bfb9cf50
aKarampetsou <anastasiakarampetsou@gmail.com>
2024-10-31 19:49:06 +0200

    Setup main entry point for React application

---
+++ src/index.js
@@ -0,0 +1,15 @@
+// src/index.js
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import CssBaseline from '@mui/material/CssBaseline';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <BrowserRouter>
+    <CssBaseline /> {/* styling στο UI */}
+    <App />
+  </BrowserRouter>
+);
