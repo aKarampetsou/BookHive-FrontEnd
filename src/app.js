119bf7685fe64c76a7b70d3b5b01c9681c208649
aKarampetsou <anastasiakarampetsou@gmail.com>
2024-11-01 22:15:08 +0200

    Added functionallity for protected routes

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
@@ -7,30 +7,64 @@ import AuthorsPage from './pages/AuthorsPage';
 import BooksPage from './pages/BooksPage';
 import BookDetailPage from './pages/BookDetailPage';
 import AuthorDetailPage from './pages/AuthorDetailPage';
-import { AuthProvider } from './context/AuthContext'; // Εισάγουμε τον AuthProvider για το context του authentication
+import { AuthProvider, AuthContext } from './context/AuthContext';
 
-// Το app.js κύριο component της εφαρμογής κάτι σαν σημείο εκκίνησης 
+// Το PrivateRoute είναι συνάρτηση που ελέγχει αν ο χρήστης είναι συνδεδεμένος ώστε να παρέχει προστασία για διαδρομές (routes) που απαιτούν σύνδεση
+function PrivateRoute({ children }) {
+  const { user } = React.useContext(AuthContext); // Ελέγχουμε αν υπάρχει συνδεδεμένος χρήστης
+  return user ? children : <Navigate to="/login" />; // Ανακατεύθυνση στο login αν ο χρήστης δεν είναι συνδεδεμένος
+}
+
 function App() {
   return (
     /*AuthProvider: Εξασφαλίζει ότι τα δεδομένα του authentication (π.χ., αν ο χρήστης είναι συνδεδεμένος και το token)
      είναι διαθέσιμα σε κάθε μέρος της εφαρμογής που το χρειάζεται (component)*/
-    <AuthProvider>
-      {/* Router: Χρησιμοποιείται για να καθοδηγούμε την εφαρμογή στα σωστά components με βάση το URL */}
-      <Router>
-        <Navbar /> {/* Το navigation bar που εμφανίζεται σε όλες τις σελίδες */}
-
-        {/* Routes: Ορίζουμε τις διαδρομές για κάθε σελίδα */}
+    <AuthProvider> 
+      <Router>  {/* Router: Χρησιμοποιείται για να καθοδηγούμε την εφαρμογή στα σωστά components με βάση το URL */}
+        <Navbar />
         <Routes>
-          <Route path="/" element={<HomePage />} /> {/* Αρχική σελίδα */}
-          <Route path="/login" element={<LoginPage />} /> {/* Σελίδα σύνδεσης */}
-          <Route path="/authors" element={<AuthorsPage />} /> {/* Λίστα συγγραφέων */}
-          <Route path="/authors/:id" element={<AuthorsDetailsPage />} /> {/* Λεπτομέρειες για συγκεκριμένο συγγραφέα */}
-          <Route path="/books" element={<BooksPage />} /> {/* Λίστα βιβλίων */}
-          <Route path="/books/:id" element={<BooksDetailsPage />} /> {/* Λεπτομέρειες για συγκεκριμένο βιβλίο */}
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          
+          {/* Προστατευμένες διαδρομές για συγγραφείς */}
+          <Route 
+            path="/authors" 
+            element={
+              <PrivateRoute> {/* */}
+                <AuthorsPage />
+              </PrivateRoute>
+            } 
+          />
+          <Route 
+            path="/authors/:id" 
+            element={
+              <PrivateRoute>
+                <AuthorDetailPage />
+              </PrivateRoute>
+            } 
+          />
+          
+          {/* Προστατευμένες διαδρομές για βιβλία */}
+          <Route 
+            path="/books" 
+            element={
+              <PrivateRoute>
+                <BooksPage />
+              </PrivateRoute>
+            } 
+          />
+          <Route 
+            path="/books/:id" 
+            element={
+              <PrivateRoute>
+                <BookDetailPage />
+              </PrivateRoute>
+            } 
+          />
         </Routes>
       </Router>
     </AuthProvider>
   );
 }
 
-export default App; 
+export default App;
