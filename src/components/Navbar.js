a61b52306be4dd76c52bfa3800db53c85b20bcae
aKarampetsou <anastasiakarampetsou@gmail.com>
2024-11-02 19:02:32 +0200

    Added conditional rendering to Navbar for login/logout functionality

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,32 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { AppBar, Toolbar, Button } from '@mui/material';
+import { Link, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../context/AuthContext';
 
 function Navbar() {
-  return (
-    <nav>
-      <Link to="/">Home</Link>
-      <Link to="/login">Login</Link>
-      <Link to="/books">Books</Link>
-      <Link to="/authors">Authors</Link>
-    </nav>
-  );
-}
+  const { user, logout } = React.useContext(AuthContext); // Η logout είναι μια συνάρτηση που έχουμε ορίσει μέσα στο AuthContext και την καλούμε για να αποσυνδεθεί ο χρήστης
+  const navigate = useNavigate(); // χρ'ηση του useNavigate για redirect στο /login μετά το logout
 
+  const handleLogout = () => {
+    logout(); // Αποσύνδεση χρήστη
+    navigate('/login'); // Ανακατεύθυνση στη σελίδα login
+  };
+
+  
+  return (
+    <AppBar position="static"> {/* Το AppBar είναι ένα component του Material UI για τη μπάρα πλοήγησης*/}
+      <Toolbar>
+        <Button color="inherit" component={Link} to="/">Home</Button> 
+        <Button color="inherit" component={Link} to="/authors">Authors</Button> 
+        {user && <Button color="inherit" component={Link} to="/books">Books</Button>} 
+        {user ? (
+          <Button color="inherit" onClick={handleLogout}>Logout</Button> // Εμφανίζει το κουμπί Logout αν ο χρήστης είναι συνδεδεμένος
+        ) : (
+          <Button color="inherit" component={Link} to="/login">Login</Button> // Εμφανίζει το κουμπί Login αν ο χρήστης δεν είναι συνδεδεμένος
+        )}
+      </Toolbar>
+    </AppBar>
+  );
+  
+}
 export default Navbar;
