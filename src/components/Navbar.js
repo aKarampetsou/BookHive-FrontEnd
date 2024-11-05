a13ad9e5daad362c9726c4b22a7231d87d813bcb
aKarampetsou <anastasiakarampetsou@gmail.com>
2024-11-05 20:32:07 +0200

    Added logout button and functionallity

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,28 +1,43 @@
-import React from 'react';
+// Navbar.js
+import React, { useContext } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { Button, AppBar, Toolbar, Typography } from '@mui/material';
+import { AuthContext } from '../context/AuthContext';
 
 function Navbar() {
+  const { logout, user } = useContext(AuthContext); // Παίρνουμε τη συνάρτηση logout και τον user από το AuthContext
   const location = useLocation();
   const navigate = useNavigate();
 
-  // Ελέγχει αν είναι στις σελίδες AuthorsPage ή BooksPage
+  // Ελέγχει αν είναι στις σελίδες AuthorsPage ή BooksPage για να εμφανίζει το κουμπί "Back"
   const showBackButton = location.pathname === '/authors' || location.pathname === '/books';
+
+  const handleLogout = () => {
+    logout(); // Εκτέλεση της αποσύνδεσης
+    navigate('/'); // Ανακατεύθυνση στην αρχική σελίδα
+  };
 
   return (
     <AppBar position="static">
       <Toolbar style={{ display: 'flex', justifyContent: 'space-between' }}>
         <Typography variant="h6">BookHive</Typography>
         
-        {showBackButton && (
-          <Button
-            color="inherit"
-            onClick={() => navigate(-1)} // Πηγαίνει πίσω στην προηγούμενη σελίδα
-            style={{ marginRight: '1rem' }}
-          >
-            Back
-          </Button>
-        )}
+        <div>
+          {showBackButton && (
+            <Button
+              color="inherit"
+              onClick={() => navigate(-1)} // Πηγαίνει πίσω στην προηγούμενη σελίδα
+              style={{ marginRight: '1rem' }}
+            >
+              Back
+            </Button>
+          )}
+          {user && ( // Εμφάνιση του κουμπιού Logout μόνο αν υπάρχει χρήστης
+            <Button color="inherit" onClick={handleLogout}>
+              Logout
+            </Button>
+          )}
+        </div>
       </Toolbar>
     </AppBar>
   );
